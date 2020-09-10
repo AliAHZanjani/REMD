15b00e65c98a42ebfc8d06c79333f7922c60f75c
Josh Berryman <the.real.josh.berryman@gmail.com>
2020-09-10 15:14:06 +0200

    added test files

--- test/testNab.bash
+++ test/testNab.bash
@@ -7,4 +7,4 @@ cp FTIYSLLL.rst.bak FTIYSLLL.rst
 ##do the run.
 ../peptide1 -t 0.01 -prStp 1 -iniConf FTIYSLLL.rst -iniTop FTIYSLLL_mb3.top \
 	    -Traj test.trj -outf out.log -Pot pot.dat -lseq testLseq.dat -lseqAd testLseq_ad.dat \
-	    -gh FTIYSLLL.pdb -sy FTIYSLLL.pdb
+	    -gh FTIYSLLL.pdb -sy FTIYSLLL.pdb -S 88779876
